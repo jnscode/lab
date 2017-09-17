ed4ab5e6cb1a769f2dd2afbba12eccf93948b6fa
jn <jn.life@qq.com>
2017-09-17 14:14:06 +0800

    add tree search

--- tree.go
+++ tree.go
@@ -1,13 +1,78 @@
 package tree
 
+import (
+	"container/list"
+)
+
+type Type int // interface{}
+
 type Node struct {
-	value int
+	value Type
 	left  *Node
 	right *Node
 }
 
-type Tree *Node
-
-func NewNode(v int) *Node {
+func NewNode(v Type) *Node {
 	return &Node{value: v}
+}
+
+func (t *Node) DeepSearch(v Type) *Node {
+	if t == nil {
+		println("root is nil")
+		return nil
+	}
+
+	println("check value", t.value)
+	if t.value == v {
+		println("find it")
+		return t
+	}
+
+	println("search left node")
+	l := t.left.DeepSearch(v)
+	if l != nil {
+		return l
+	}
+
+	println("search right node")
+	r := t.right.DeepSearch(v)
+	if r != nil {
+		return r
+	}
+
+	return nil
+}
+
+func (t *Node) BreadthSearch(v Type) *Node {
+	if t == nil {
+		println("root is nil")
+		return nil
+	}
+
+	q := list.New()
+	q.PushBack(t)
+
+	for i := q.Front(); i != nil; {
+		n := i.Value.(*Node)
+		println("check value", n.value)
+
+		if n.value == v {
+			println("find it")
+			return n
+		}
+
+		if n.left != nil {
+			q.PushBack(n.left)
+		}
+
+		if n.right != nil {
+			q.PushBack(n.right)
+		}
+
+		cur := i
+		i = i.Next()
+		q.Remove(cur)
+	}
+
+	return nil
 }
--- tree_test.go
+++ tree_test.go
@@ -4,6 +4,20 @@ import (
 	"testing"
 )
 
+func CreateTree() *Node {
+	n0 := NewNode(1)
+	n1 := NewNode(2)
+	n2 := NewNode(3)
+	n3 := NewNode(4)
+	n4 := NewNode(5)
+
+	n0.left = n1
+	n0.right = n2
+	n1.left = n3
+	n1.right = n4
+	return n0
+}
+
 func TestCreateNode(t *testing.T) {
 	n := NewNode(1)
 	if n == nil {
@@ -21,4 +35,16 @@ func TestCreateTree(t *testing.T) {
 	r := NewNode(1)
 	r.left = NewNode(2)
 	r.right = NewNode(3)
+}
+
+func TestDeepSearch(t *testing.T) {
+	r := CreateTree()
+	n := r.DeepSearch(3)
+	println(n)
+}
+
+func TestBreadthSearch(t *testing.T) {
+	r := CreateTree()
+	n := r.BreadthSearch(5)
+	println(n)
 }
